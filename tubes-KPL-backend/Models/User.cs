4a880ab3ef81747e43e4d8b96b92a46246952db5
BbayuGt <49893593+BbayuGt@users.noreply.github.com>
2026-05-11 22:59:58 +0700

    Add Role to User

--- tubes-KPL-backend/Models/User.cs
+++ tubes-KPL-backend/Models/User.cs
@@ -10,4 +10,5 @@ public class User
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = "User";
 }
