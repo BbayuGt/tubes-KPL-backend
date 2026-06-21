76533afaa0318505c3db61ce874dd0ff103abbb4
Erleazar Pandita Ramadhan <erleazar02@gmail.com>
2026-06-22 01:42:38 +0700

    migrate

---
+++ tubes-KPL-backend/Migrations/20260621184148_UpdateWebhookProgress.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tubes_KPL_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateWebhookProgress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Donations_Users_UserId",
+                table: "Donations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Donations_UserId",
+                table: "Donations");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Donations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DonorEmail",
+                table: "Donations",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DonorName",
+                table: "Donations",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DonorEmail",
+                table: "Donations");
+
+            migrationBuilder.DropColumn(
+                name: "DonorName",
+                table: "Donations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Donations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Donations_UserId",
+                table: "Donations",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Donations_Users_UserId",
+                table: "Donations",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
